8f7e47cea99af69e7f651253b8bba280775b9679
Timothy Oltjenburns <timothyolt@gmail.com>
2013-12-16 13:21:20 -0500

    Added rendering for future pickup class (comments under Player)

--- core/src/main/java/org/bytefire/ld28/core/Player.java
+++ core/src/main/java/org/bytefire/ld28/core/Player.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import org.bytefire.ld28.core.asset.Sprite;
+import org.bytefire.ld28.core.asset.SpriteHandler;
 import org.bytefire.ld28.core.screen.AbstractScreen;
 import org.bytefire.ld28.core.screen.GameScreen;
 
@@ -54,6 +55,23 @@ public class Player extends Actor implements CollisionManager{
         Color color = getColor();
         batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
         batch.draw(tex, getX() - (tex.getRegionWidth() / 2), getY() - (tex.getRegionHeight() / 2));
+
+                SpriteHandler s = game.getSpriteHandler();
+        float maxtime = 6f;
+        float time = 2.5f;
+        int height = s.getRegion(Sprite.FLY_OFF).getRegionHeight();
+        int midpoint = Math.round(time / maxtime * height);
+
+//        TextureRegion top = s.getRegion(Sprite.FLY_OFF);
+//        top.setRegionHeight(height - midpoint);
+//        TextureRegion bot = s.getRegion(Sprite.FLY_ON);
+//        bot.setRegionY(bot.getRegionY() + height - midpoint);
+//        bot.setRegionHeight(midpoint);
+//
+//        Color color = getColor();
+//        batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
+//        batch.draw(top, getX() - (top.getRegionWidth() / 2), getY() - (height / 2) + midpoint);
+//        batch.draw(bot, getX() - (bot.getRegionWidth() / 2), getY() - (height / 2));
     }
 
     @Override
--- core/src/main/java/org/bytefire/ld28/core/asset/Sprite.java
+++ core/src/main/java/org/bytefire/ld28/core/asset/Sprite.java
@@ -9,17 +9,16 @@ public enum Sprite {
     PLAYER(Gdx.files.internal("spritesheet.png"), 16, 16, 0),
     LINE(Gdx.files.internal("spritesheet.png"), 16, 8, 1),
     LINECAP(Gdx.files.internal("spritesheet.png"), 16, 8, 7),
-    BOUNCE_ON(Gdx.files.internal("spritesheet.png"), 8, 8, 3),
-    BOUNCE_OFF(Gdx.files.internal("spritesheet.png"), 8, 8, 7),
-    MAGNET_ON(Gdx.files.internal("spritesheet.png"), 8, 8, 4),
-    MAGNET_OFF(Gdx.files.internal("spritesheet.png"), 8, 8, 8),
-    FLY_ON(Gdx.files.internal("spritesheet.png"), 8, 8, 5),
-    FLY_OFF(Gdx.files.internal("spritesheet.png"), 8, 8, 9),
-    GRAV_ON(Gdx.files.internal("spritesheet.png"), 8, 8, 6),
-    GRAV_OFF(Gdx.files.internal("spritesheet.png"), 8, 8, 10),
-    SIZE_ON(Gdx.files.internal("spritesheet.png"), 8, 8, 7),
-    sIZE_OFF(Gdx.files.internal("spritesheet.png"), 8, 8, 11);
-
+    BOUNCE_ON(Gdx.files.internal("spritesheet.png"), 16, 16, 2),
+    BOUNCE_OFF(Gdx.files.internal("spritesheet.png"), 16, 16, 7),
+    MAGNET_ON(Gdx.files.internal("spritesheet.png"), 16, 16, 3),
+    MAGNET_OFF(Gdx.files.internal("spritesheet.png"), 16, 16, 8),
+    FLY_ON(Gdx.files.internal("spritesheet.png"), 16, 16, 4),
+    FLY_OFF(Gdx.files.internal("spritesheet.png"), 16, 16, 9),
+    GRAV_ON(Gdx.files.internal("spritesheet.png"), 16, 16, 5),
+    GRAV_OFF(Gdx.files.internal("spritesheet.png"), 16, 16, 10),
+    SIZE_ON(Gdx.files.internal("spritesheet.png"), 16, 16, 6),
+    SIZE_OFF(Gdx.files.internal("spritesheet.png"), 16, 16, 11);
 
     public final FileHandle file;
     public final int width;
